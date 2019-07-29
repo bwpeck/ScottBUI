5381ce60a74606537106e6e05f8bb34e60b5c529
dangolbeeker <scottb305@outlook.com>
2019-07-29 18:40:18 -0400

    added .env file with api key testing in console log *Never do This For Client Insecure*

--- youtubeu/src/App.js
+++ youtubeu/src/App.js
@@ -1,10 +1,13 @@
-import React, {useState} from 'react'
+import React, {useState} from 'react';
+import {Container} from 'react-bootstrap';
 import './App.css';
 
 
 function App() {
   return (
     <div className="App">
+      const API_KEY =`${process.env.REACT_APP_API_KEY_YT}`
+      console.log("API", API_KEY)
      
      
     </div>
