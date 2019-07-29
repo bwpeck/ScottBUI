0a58073445aca3f97eaf92e3e0d3fe7d33a80415
Matt Herich <mjherich@gmail.com>
2019-07-29 11:51:46 -0700

    Add header and footer

--- youtubeu/src/App.js
+++ youtubeu/src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Menu from './Menu';
+import Header from './components/header';
+import StickyFooter from './components/footer';
 
 
 function App() {
   return (
     <div className="App">
-     
+      <Header />
+      <StickyFooter />
     </div>
   );
 }
