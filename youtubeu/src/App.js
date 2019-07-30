b8269b39a8299a17a4a534b340b72e34cddb18b7
dangolbeeker <scottb305@outlook.com>
2019-07-30 15:21:24 -0400

    added search video component/working on adding fetch hook and passing props to videolist component

--- youtubeu/src/App.js
+++ youtubeu/src/App.js
@@ -1,17 +1,34 @@
 import React, {useState} from 'react';
 import {Container} from 'react-bootstrap';
 import './App.css';
+import useFetch from './components/apifetchhook;'
+import {VDetails} from './components/VDetails';
+import {VideoList} from './components/VideoList';
+import {SearchVideo} from './components/SearchVideo';
 
+const App = () => {
+  const [videoName,
+  setVideoName] = useState('DIY');
+  const [data,
+  isLoading,
+isError,
+setUrl] = useFetch(`/search/?q=${videoName}`, {defaultResponse: []});
 
-function App() {
-  return (
-    <div className="App">
-      const API_KEY =`${process.env.REACT_APP_API_KEY_YT}`
-      console.log("API", API_KEY)
-     
-     
-    </div>
-  );
+const handleFormSubmit = (e) => {
+  e.preventDefault();
+  if (!videoName.trim().length) 
+  return;
+setUrl(`/search/?q=${videoName}`);
+setVideoName('');
+
 }
 
-export default App;
+const handleChange = (e) => {
+  const {value} = e.target;
+  setVideoName(value);
+}
+
+
+return (
+  <container></container>
+)
---
+++ youtubeu/src/components/SearchVideo.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Form} from 'react-bootstrap';
+
+
+export const SearchVideo = ({handleFormSubmit, handleChange, videoName}) => {
+    return (
+        <form className="video-form" onSubmit={handleFormSubmit}>
+            <Form.Control
+            name="video"
+            placeholder="Learn Something New On YouTube U.."
+           className="form-control video-form_input mt-4"
+           onChange={handleChange}
+           value={videoName}/>
+           <button type="submit" className="video-form_btn" variant="primary">Lets Learn</button>
+           </form>
+    )
+}
