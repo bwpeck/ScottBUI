96a21d2f9aa1712bfc579fe24698e59699ce5afa
dangolbeeker <scottb305@outlook.com>
2019-07-30 14:50:37 -0400

    fetch component finished

--- youtubeu/src/components/apifetchhook.js
+++ youtubeu/src/components/apifetchhook.js
@@ -1,10 +1,53 @@
 import {useState, useEffect} from 'react';
 import api from './api';
-
 const YT_API_KEY = process.env.REACT_APP_YT_API_KEY;
 
-
-
 const useFetch = (initialURL, params) => {
-    const
+    const {defaultResponse} = params;
+    const [data,
+    setData] = useState(defaultResponse);
+    const [url,
+    setUrl] = useState(initialURL);
+    const [isLoading,
+    setIsLoading] = useState(false);
+    const [isError,
+    setIsError] = useState(false);
+
+    useEffect(() => {
+        const doFetch = () => {
+            setIsError(false);
+            setIsLoading(true);
+            setUrl(url);
+
+            api
+                .get(url, {
+                params: {
+                    part: 'snippet',
+                    type: 'video',
+                    maxResults: 5,
+                    key: YT_API_KEY
+                }
+            })
+                .catch(error => {
+                    setIsError(true);
+                    if (error.response) {
+                        throw new Error(`Error: ${JSON.stringify(error.message)}`);
+                    } else if (error.request) {
+                        throw new Error(`Error: No response from the server`);
+                    } else {
+                        throw new Error('Error', error.message);
+                    }
+                })
+                .then(response => {
+                    setData(response.data.items)
+                    setIsLoading(false);
+                });
+        }
+
+        doFetch();
+    }, [url]);
+
+return [data, isLoading, isError, setUrl];
 }
+
+export default useFetch;
