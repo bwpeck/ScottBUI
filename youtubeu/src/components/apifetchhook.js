f9c300ceece06ec8a39edb0c50f1ef377025ce11
dangolbeeker <scottb305@outlook.com>
2019-07-30 12:41:42 -0400

    building custom hook to fetch api

---
+++ youtubeu/src/components/apifetchhook.js
@@ -0,0 +1,10 @@
+import {useState, useEffect} from 'react';
+import api from './api';
+
+const YT_API_KEY = process.env.REACT_APP_YT_API_KEY;
+
+
+
+const useFetch = (initialURL, params) => {
+    const
+}
