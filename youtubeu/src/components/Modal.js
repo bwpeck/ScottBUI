ab7876b8055a1290977724484ce87064bb7eb891
Matt Herich <mjherich@gmail.com>
2019-07-31 15:23:05 -0700

    Fix video responsiveness in modal

--- youtubeu/src/components/Modal.js
+++ youtubeu/src/components/Modal.js
@@ -23,9 +23,12 @@ export default function VideoModal({videoId, title, open, handleClose}) {
                 <DialogTitle id="alert-dialog-slide-title">{title}</DialogTitle>
                 <DialogContent>
                     <DialogContentText id="alert-dialog-slide-description">
-                        <iframe id="ytplayer" type="text/html" width="640" height="360"
-                                src={`https://www.youtube.com/embed/${videoId}?autoplay=1`}
-                                frameborder="0"></iframe>
+                        <div style={{width: '100%', height: '0', paddingBottom: '56.25%', position: 'relative'}}>
+                          <iframe style={{position: 'absolute', width: '100%', height: '100%'}}
+                                  id="ytplayer" type="text/html" width="640" height="360"
+                                  src={`https://www.youtube.com/embed/${videoId}?autoplay=1`}
+                                  frameborder="0"></iframe>
+                        </div>
                     </DialogContentText>
                 </DialogContent>
             </Dialog>
